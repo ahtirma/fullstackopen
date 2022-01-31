3d75ff317bad73b3b25e5431a9c2954e06de6eb5
Amritha Govindarajan <g.amritha@gmail.com>
2022-01-31 21:51:00 +0200

    2.5: separate module

--- part02/courseinfo/src/App.js
+++ part02/courseinfo/src/App.js
@@ -1,47 +1,5 @@
-import React from 'react';
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course} />
-    </div>
-  )
-}
-
-const Header = ({course}) => (
-  <h2>{course.name}</h2>
-);
-
-const Part = ({part}) => (
-  <p>{part.name} {part.exercises}</p>
-)
-
-const Content = ({course}) => {
-  const parts = course.parts;
-  return(
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-)};
-
-const Total = ({course}) => {
-  const parts = course.parts;
-  
-  const exercisesArray = parts.map(part => part.exercises);
-
-  const total = exercisesArray.reduce(
-    (previousValue, currentValue) => previousValue + currentValue
-  );
-
-  return(
-    <p>
-      <b>
-      Total of {total} exercises
-      </b>
-    </p>
-)};
+import React from 'react';      
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
